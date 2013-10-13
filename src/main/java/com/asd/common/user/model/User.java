40088fcf3a733bcd6906ca9aaf8045fa344e6ff3
skiplist <skiplist@users.noreply.github.com>
2013-10-12 21:28:25 -0400

    removing password

--- src/main/java/com/asd/common/user/model/User.java
+++ src/main/java/com/asd/common/user/model/User.java
@@ -24,7 +24,6 @@ public class User {
 	private String firstName;
 	private String lastName;
 	private String email;
-	private String password;
 
 	public String getFirstName() {
 		return firstName;
@@ -48,14 +47,6 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 }
